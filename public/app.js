792a4cca3b36707bffdc86dfc19deeaed820c101
gorgwallis <pianobirde@gmail.com>
2022-07-13 12:17:40 -0600

    added frontend variables

--- public/app.js
+++ public/app.js
@@ -1,4 +1,23 @@
 var app = new Vue({
- el: "#app",
- vuetify: new Vuetify(),
+    el: "#app",
+    vuetify: new Vuetify(),
+    data: {
+        page: "landing",
+
+        usernameInput: "",
+        passwordInput: "",
+
+        newUsername: "",
+        newPassword:"",
+        newEmail:"",
+        newPhoneNumber: "",
+
+        role: "",
+        pageView: "",
+    },
+
+    methods: {
+        
+    }
+
 })
