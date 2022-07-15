930363d3a4fa2472ea6299b1b7aa2e01ed2adfd8
root <root@DESKTOP-2UAM98D.localdomain>
2022-07-15 10:50:47 -0600

    Add Sign in ability

--- public/app.js
+++ public/app.js
@@ -38,7 +38,40 @@ var app = new Vue({
                 console.log("Error logging in, status: "+ response.status);
             }
 
+        },
+        
+        postSession: async function () {
+            if (this.usernameInput == "" || this.passwordInput == "") {
+                console.log("Username or Password field is empty");
             }
-        }
+            let loginCredentials = {
+                username: this.usernameInput, 
+                password: this.passwordInput
+            };
+            let response = await fetch(URL + "/session", {
+                method: "POST",
+                body: JSON.stringify(loginCredentials),
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                credentials: "include"
+            });
 
+            //Parse response data
+            let body = await response.json();
+
+            //Check for successful login
+            if (response.status == 201) {
+                //Succesful login
+                console.log("Successful login attempt");
+                this.usernameInput = "";
+                this.passwordInput = "";
+            } else if (response.status == 401) {
+                console.log ("Unsuccesful login attempt")
+                this.loginPassWord = "";
+            } else {
+                console.log("Some sort of error when POST /session");
+            }
+        },
+    }
 });
