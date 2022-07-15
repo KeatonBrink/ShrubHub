b29d638d79a69d5c0dbd9163cbcd1fd98e41d4ae
root <root@DESKTOP-2UAM98D.localdomain>
2022-07-15 11:28:41 -0600

    fix more vue issues

--- public/app.js
+++ public/app.js
@@ -35,26 +35,28 @@ var app = new Vue({
                 this.page = "mowermain";
             } else if (response.status == 401) {
                 console.log("Incorrect username or password. Try again.");
+                this.page = "landing-page";
             } else {
+                this.page = "landing-page";
                 console.log("Error logging in, status: "+ response.status);
             }
         },
 
-        // createUser: async function () {
-        //     if (this.newUsernameInput != null) {
-        //         if (this.newPasswordInput != null) {
-        //             if (this.newPasswordInput == this.newPasswordInput2) {
-        //                 if (this.newEmailInput != null) {
-        //                     if (this.newDefaultRole != null) {
-        //                         console.log("Credentials complete. Account created.")
+        createUser: async function () {
+            if (this.newUsernameInput != null) {
+                if (this.newPasswordInput != null) {
+                    if (this.newPasswordInput == this.newPasswordInput2) {
+                        if (this.newEmailInput != null) {
+                            if (this.newDefaultRole != null) {
+                                console.log("Credentials complete. Account created.")
 
-        //                     } else { console.log("Please select Mower or Poster role.")};
-        //                 } else {console.log("Please insert your email address.")};
-        //             } else {console.log("Password inputs do not match. Re-type your password.")};
-        //         } else {console.log("Please insert a password")};
-        //     } else {console.log("Please insert a username.")};
+                            } else { console.log("Please select Mower or Poster role.")};
+                        } else {console.log("Please insert your email address.")};
+                    } else {console.log("Password inputs do not match. Re-type your password.")};
+                } else {console.log("Please insert a password")};
+            } else {console.log("Please insert a username.")};
             
-        // }
+        },
         
     
         
